e9e5d9a5af8be38ca6f78708654645c8cbee5cc6
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-01-31 20:11:52 +0800

    clean

--- neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/RyoamicLightsNeoForge.java
+++ neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/RyoamicLightsNeoForge.java
@@ -11,7 +11,11 @@
 package org.thinkingstudio.ryoamiclights.neoforge;
 
 import dev.architectury.platform.Platform;
+import net.minecraft.client.MinecraftClient;
+import net.neoforged.bus.api.EventPriority;
 import net.neoforged.fml.loading.FMLLoader;
+import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
+import net.neoforged.neoforge.common.NeoForge;
 import org.thinkingstudio.ryoamiclights.RyoamicLights;
 import net.neoforged.fml.common.Mod;
 import org.thinkingstudio.ryoamiclights.gui.SettingsScreen;
@@ -26,6 +30,13 @@ public class RyoamicLightsNeoForge {
 
     public void onInitializeClient() {
         new RyoamicLights().clientInit();
+
+        NeoForge.EVENT_BUS.addListener(EventPriority.HIGHEST, RenderLevelStageEvent.class, event -> {
+            if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_TRIPWIRE_BLOCKS) {
+                MinecraftClient.getInstance().getProfiler().swap("dynamic_lighting");
+                RyoamicLights.get().updateAll(event.getLevelRenderer());
+            }
+        });
 
         Platform.getMod(RyoamicLights.NAMESPACE).registerConfigurationScreen(SettingsScreen::new);
     }
