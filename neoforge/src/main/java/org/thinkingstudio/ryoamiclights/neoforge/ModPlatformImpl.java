056f3dead50e371f85bd73e76c6998335aa18578
TexTrue <3140846162@qq.com>
2024-06-20 16:21:33 +0800

    fix

--- neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/ModPlatformImpl.java
+++ neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/ModPlatformImpl.java
@@ -1,13 +1,13 @@
 package org.thinkingstudio.ryoamiclights.neoforge;
 
-import net.neoforged.fml.ModList;
+import net.neoforged.fml.loading.FMLLoader;
 import net.neoforged.fml.loading.FMLPaths;
 
 import java.nio.file.Path;
 
 public class ModPlatformImpl {
     public static boolean isModLoaded(String modid) {
-        return ModList.get().isLoaded(modid);
+        return FMLLoader.getLoadingModList().getModFileById(modid) != null;
     }
 
     public static Path getConfigDir() {
