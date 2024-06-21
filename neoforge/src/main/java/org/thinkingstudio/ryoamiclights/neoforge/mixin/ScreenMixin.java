d939cc38d175ac1e0e2980da3b4bbeb057128294
TexTrue <3140846162@qq.com>
2024-06-21 23:50:37 +0800

    update NeoForge version, (NeoForge) port `VideoOptionsScreenMixin` from Fabric side

---
+++ neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/mixin/ScreenMixin.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2024 ThinkingStudio
+ *
+ * This file is part of RyoamicLights.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
+package org.thinkingstudio.ryoamiclights.neoforge.mixin;
+
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.thinkingstudio.obsidianui.Tooltip;
+
+@Mixin(Screen.class)
+public class ScreenMixin {
+    @Inject(method = "render", at = @At(value = "TAIL"))
+    private void onRender(DrawContext graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        if (((Screen) (Object) this) instanceof VideoOptionsScreen) {
+            Tooltip.renderAll(graphics);
+        }
+    }
+}
---
+++ neoforge/src/main/java/org/thinkingstudio/ryoamiclights/neoforge/mixin/VideoOptionsScreenMixin.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright © 2020~2024 LambdAurora <email@lambdaurora.dev>
+ * Copyright © 2024 ThinkingStudio
+ *
+ * This file is part of RyoamicLights.
+ *
+ * Licensed under the MIT license. For more information,
+ * see the LICENSE file.
+ */
+
+package org.thinkingstudio.ryoamiclights.neoforge.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.GameOptionsScreen;
+import net.minecraft.client.gui.screen.option.VideoOptionsScreen;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.Text;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.thinkingstudio.ryoamiclights.gui.DynamicLightsOptionsOption;
+
+@Mixin(VideoOptionsScreen.class)
+public abstract class VideoOptionsScreenMixin extends GameOptionsScreen {
+	@Unique
+	private SimpleOption<?> ryoamiclights$option;
+
+	public VideoOptionsScreenMixin(Screen parent, GameOptions gameOptions, Text title) {
+		super(parent, gameOptions, title);
+	}
+
+	@Inject(method = "<init>", at = @At("TAIL"))
+	private void onConstruct(Screen parent, MinecraftClient client, GameOptions gameOptions, CallbackInfo ci) {
+		this.ryoamiclights$option = DynamicLightsOptionsOption.getOption(this);
+	}
+
+	@ModifyArg(
+			method = "addOptions",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/widget/OptionListWidget;addAll([Lnet/minecraft/client/option/SimpleOption;)V"
+			),
+			index = 0
+	)
+	private SimpleOption<?>[] addOptionButton(SimpleOption<?>[] old) {
+		var options = new SimpleOption<?>[old.length + 1];
+		System.arraycopy(old, 0, options, 0, old.length);
+		options[options.length - 1] = this.ryoamiclights$option;
+		return options;
+	}
+}
